23b945a48e8916bd7e9894f4842fcee3cb8e9843
Babini Stefano <stefano.babini5@studio.unibo.it>
2020-12-01 12:18:42 +0100

    Finish

--- src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
+++ src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
@@ -1,6 +1,4 @@
 package it.unibo.oop.lab.workers02;
-
-import java.util.List;
 import java.util.stream.IntStream;
 
 
@@ -22,58 +20,18 @@ public class MultiThreadedSumMatrix implements SumMatrix {
         return  IntStream.iterate(0, start -> (int) start + size)
                 .limit(nthread)
                 .parallel()
-                .map(startpos -> {
-                    System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
-                    for (int i = startpos; i < list.size() && i < startpos + nelem; i++) {
-                        this.res += this.list.get(i);
+                .mapToDouble(startpos -> {
+                    double res = 0;
+                    System.out.println("Working from position " + startpos + " to position " + (startpos + size - 1));
+                    for (int i = startpos; i < matrix.length && i < startpos + size; i++) {
+                        for (final double d : matrix [i]) {
+                            res += d;
+                        }
                     }
+                    return res;
                 })
                 .sum();
     }
 
-
-    }
-
-    private static class Worker extends Thread {
-        private final List<Double> list;
-        private final int startpos;
-        private final int nelem;
-        private double res;
-
-        /**
-         * Build a new worker.
-         * 
-         * @param list
-         *            the list to sum
-         * @param startpos
-         *            the initial position for this worker
-         * @param nelem
-         *            the no. of elems to sum up for this worker
-         */
-        Worker(final List<Double> list, final int startpos, final int nelem) {
-            super();
-            this.list = list;
-            this.startpos = startpos;
-            this.nelem = nelem;
-        }
-
-        @Override
-        public void run() {
-            System.out.println("Working from position " + startpos + " to position " + (startpos + nelem - 1));
-            for (int i = startpos; i < list.size() && i < startpos + nelem; i++) {
-                this.res += this.list.get(i);
-            }
-        }
-
-        /**
-         * Returns the result of summing up the integers within the list.
-         * 
-         * @return the sum of every element in the array
-         */
-        public double getResult() {
-            return this.res;
-        }
-
-    }
 
 }
